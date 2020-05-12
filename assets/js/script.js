3d2f32890b1e35987c836e0605119a056e49c3d6
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-12 22:04:59 +0200

    Corrigé8

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
+// Je cible mon bouton masqué grâce à son ID. Et je lui rajoute un évènement au clique.
+// au clique sur mon bouton, je cache mon texte qui a l'ID 'text'.
 $(function(){
-//Empty vide le contenu de l'élément ciblé, et REMOVE supprime l'élément ciblé.
-$('#firstText').empty();
-$('#secondText').remove();
+  $("#hide").click(function(){
+    $("#text").hide();
+  });
+// Je cible mon bouton masqué grâce à son ID. Et je lui rajoute un évènement au clique.
+// au clique sur mon bouton, j'affiche mon texte qui a l'ID 'text'.
+  $("#show").click(function(){
+    $("#text").show();
+  });
 });
